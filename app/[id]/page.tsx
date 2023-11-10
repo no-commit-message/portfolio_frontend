cc69142610dd41675ed53f930ba98506a3f563e1
川原歩夢 <ayumu@kawaharumenoAir.AirPort>
2023-11-10 12:11:31 +0900

    Update layout of detail pages

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -20,15 +20,17 @@ export default async function Detail({ params }: { params: { id: string } }) {
         <div>
             <Header />
             <div className="flex justify-center mt-8">
-                <div className="w-6/12 border rounded-lg shadow-lg flex flex-col px-8 py-4">
-                    <h1 className="border-b border-b-slate-300 text-2xl mb-4">{ post.title }</h1>
-                    <p>{ post.content }</p>
-                    <p className="mt-2">自信: {post.skill_level}/5</p>
-                    <div className="flex">
+                <div className="w-6/12 border rounded-lg shadow-lg flex flex-col px-10 py-6">
+                    <h1 className="border-b border-b-slate-300 text-3xl mb-8">{ post.title }</h1>
+                    <div className="flex mb-6">
                         <p className="mr-4">投稿日: { post.created_at }</p>
                         <p>更新日: { post.updated_at }</p>
                     </div>
-                    <Link href='/' className="hover:text-blue-900 hover:underline mt-1 text-blue-700">投稿一覧に戻る</Link>
+                    <div className="text-xl">
+                        <p>{ post.content }</p>
+                        <p className="my-6">自信: {post.skill_level}/5</p>
+                        <Link href='/' className="hover:text-blue-900 hover:underline text-blue-700">投稿一覧に戻る</Link>
+                    </div>
                 </div>
             </div>
             <Footer />
